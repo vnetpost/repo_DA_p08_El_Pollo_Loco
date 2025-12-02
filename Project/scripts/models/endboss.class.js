3210f4c086300d3bce591e78249bc42553618d33
vnetpost <vnetpost@gmail.com>
2025-12-02 11:40:25 +0000

    updated

--- Project/scripts/models/endboss.class.js
+++ Project/scripts/models/endboss.class.js
@@ -6,8 +6,19 @@ export class Endboss extends MovableObject {
     // #region Attributes
     width = 200;
     height = 250;
-    x = 500;
+    x = 720 * 4 - this.width;                   // Jeja Start Position
     y = 200;
+
+    speed = 5;
+    alertRange = 720 / 3;
+    attackRange = 720 / 4;
+    alertDuration = 1000;                       // ms to show alert frames before walking
+
+    sinceAlerted;                               // MS
+    sinceAttacking;                             // MS
+    isActivated = false;
+
+    // distance;                                   // distance With Pepe (always positiv)
 
     offset = {
         top: this.height / 5,
@@ -16,43 +27,114 @@ export class Endboss extends MovableObject {
         right: this.width / 7
     };
 
-    IMAGES_ALERT = ImgHub.IMGS.boss.alert;
+    // isDead = false;                          // In MovableObject
+    status = {
+        isWalking: false,
+        isAlerted: false,
+        isAttacking: false,
+        isHurt: true,
+    };
+
     IMAGES_WALKING = ImgHub.IMGS.boss.walk;
+    IMAGES_ALERT = ImgHub.IMGS.boss.alert;
+    IMAGES_ATTACK = ImgHub.IMGS.boss.attack;
     IMAGES_HURT = ImgHub.IMGS.boss.hurt;
     IMAGES_DEAD = ImgHub.IMGS.boss.dead;
-    IMAGES_ATTACK = ImgHub.IMGS.boss.attack;
 
     // #endregion Attributes
 
     constructor() {
         super();
-        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadImage(this.IMAGES_WALKING[3]);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_ALERT);
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
+
         IntervalHub.startInterval(this.animate, 1000 / 10);
-        // IntervalHub.startInterval(this.moveLeft, 1000 / 60);
-        // IntervalHub.startInterval(this.moveRight, 1000 / 60);
+        IntervalHub.startInterval(this.moveLeft, 1000 / 10);
+        IntervalHub.startInterval(this.updateBehavior, 1000 / 60);
         IntervalHub.startInterval(this.getRealFrame, 1000 / 60);
     }
 
     // #region Instance Methods
 
-    animate = () => this.playAnimation(this.IMAGES_WALKING);
+    animate = () => {
+        if (this.isDead) {
+            this.setAnimation(this.IMAGES_DEAD);
+            return;
+        }
+        if (this.isHurt()) { // Hurt because of lastHit-Time
+            this.setAnimation(this.IMAGES_HURT);
+            this.world.statusBar_endboss.setPrecentage(this.energy);
+            return;
+        }
+        if (this.status.isAttacking) {
+            this.setAnimation(this.IMAGES_ATTACK);
+            return;
+        }
+        if (this.status.isAlerted) {
+            this.setAnimation(this.IMAGES_ALERT);
+            return;
+        }
+        if (this.status.isWalking) {
+            this.setAnimation(this.IMAGES_WALKING);
+            return;
+        }
+    }
 
     moveLeft = () => {
-        if (this.world.keyboard.LEFT) {
-            this.otherDirection = true;
-            this.x -= this.speed;
-            if (this.x <= 0) this.x = 0;
-        }
-
+        if (!this.isActivated || this.status.isAlerted || this.status.isAttacking || this.isDead) return;
+        this.x -= this.speed;
+        if (this.x <= 0) this.x = 0;
     }
 
-    moveRight = () => {
-        if (this.world.keyboard.RIGHT) {
-            this.otherDirection = false;
-            this.x += this.speed;
-            if (this.x + this.width >= 720 * 4) this.x = 720 * 4 - this.width;
+    hit = () => {
+        this.energy > 0 ? this.energy -= 10 : this.isDead = true;
+        this.status.isHurt = true;
+        this.lastHit = Date.now();
+    }
+
+    updateBehavior = () => {
+        // if (!this.world.character || this.isDead) return;
+
+        const distance = this.getDistance();
+        const inAttackRange = distance < this.attackRange;
+        const inAlertRange = distance < this.alertRange;
+
+        if (!inAlertRange && !inAttackRange) {  // Trigger Walk-Anim
+            this.status.isAttacking = false;
+            this.status.isAlerted = false;
+            this.status.isWalking = true;
+            return;
+        }
+        if (inAlertRange) {                     // Trigger Alert-Anim 
+            this.isActivated = true;
+
+            this.status.isAttacking = false;
+            this.status.isAlerted = true;
+            this.status.isWalking = false;
+
+            this.sinceAlerted = Date.now();
+            return;
+        }
+        if (inAttackRange) {                    // Trigger Attack-Anim
+            this.status.isAttacking = true;
+            this.status.isAlerted = false;
+            this.status.isWalking = false;
+            return;
         }
     }
+
+    getDistance() {
+        return Math.abs(
+            (this.world.camera_x + this.world.character.x + this.world.character.width / 2) -
+            (this.world.camera_x + this.x + this.width / 2)
+        );
+    }
+
+    updateSinceAlerted() { this.sinceAlerted = Date.now(); }
+    updateSinceAttaking() { this.sinceAttacking = Date.now(); }
     // #endregion Instance Methods
 }
